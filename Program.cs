2570d70675a95e69d66ade404d7e929f78d8c332
Kürşad's PC <Krad's PC@DESKTOP-URFB7VL>
2024-04-23 17:18:51 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace fibo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 1,c,sayac=1;
+            string write = "";
+            Console.WriteLine("Desired Fibonacci number?:");
+            int DesiredFibo=int.Parse(Console.ReadLine());
+            while(true)
+            {
+                c = a + b;
+                a = b; 
+                b=c;
+                if (DesiredFibo==sayac)
+                {
+                    write = DesiredFibo+". Fibonacci number:" + a;
+                    break;
+                }
+                sayac++;
+            }
+            Console.WriteLine(write );
+        } 
+    }
+}
